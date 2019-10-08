36def84830eee09c69bd83654b6729f351aa7b13
nanok-iscteiul <nanok@iscte-iul.pt>
2019-10-08 09:34:30 +0100

    2nd commit

--- Aula1/src/Main.java
+++ Aula1/src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello World!");			
+		}
 	}
 
 }
