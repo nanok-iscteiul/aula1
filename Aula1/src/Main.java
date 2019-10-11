b1a378aa0a9ce4b67aaf9b0345a8c15a7e223fd5
nanok-iscteiul <nanok@iscte-iul.pt>
2019-10-11 11:45:00 +0100

    Interface que fala com o utilizador

--- Aula1/src/Main.java
+++ Aula1/src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Hello World!");			
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Introduza o seu nome.");
+		
+		String nome = sc.next();
+		
+		System.out.println("Olá "+ nome+", bem vindo! Qual é a sua idade?");
+		
+		String aux = sc.next();
+		
+		int idade = Integer.parseInt(aux);
+		
+		if (idade>20) {
+			System.out.println("Ahhhhh, "+ nome+ ", és mesmo velho pá!");
+		}else {
+			System.out.println("Fogo! "+ nome+ ", és bué novo!");
 		}
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Bye!");
-		}
-		Barbeiro b = new Barbeiro();
-		System.out.println(b.toString());
+
 	}
 
 }
